8accd564bfd0bda3a8e0bb70b1eb8d10f6b6b503
Jose Szychowski <joseszychowski@gmail.com>
2026-02-05 12:15:26 -0300

    chore: add status as a subresource

--- pkg/apis/policy/v1alpha1/types.go
+++ pkg/apis/policy/v1alpha1/types.go
@@ -5,7 +5,8 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// +genclient
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
 // +kubebuilder:resource:scope=Cluster
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
